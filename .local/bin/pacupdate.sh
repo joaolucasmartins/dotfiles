a1467b1bfb63c1aba8f1c9004b20955919f1585b
Joao Lucas <joaolucassilvamartins@gmail.com>
2020-09-17 16:28:48 +0100

    Forgot some scripts

--- .local/bin/pacupdate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cupd=$(((checkupdates | wc -l) + (checkupdates-aur | wc -l)))
-echo "$cupd updates"
-
--- .local/bin/rand_wal.sh
+++
@@ -1,6 +0,0 @@
-while true
-do
-   sleep 1h &
-   wait
-   /home/jm/.local/bin/wal_select.sh &
-done
