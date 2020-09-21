a902571ed8dd7d27de3e00247c84e7b8d2af4040
Joao Lucas <joaolucassilvamartins@gmail.com>
2020-09-21 11:21:16 +0100

    Added suckless stuff

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # My dotfiles
 
 Still very much incomplete. Why? My name is frisk you, Costa you are my because.
+Need to build st, slock and dmenu. (included)
+
+# Dependencies
+
+pywal xmonad xmonad-contrib x
---
+++ apps/dmenu/config.h
@@ -0,0 +1,39 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+#include "/home/jm/.cache/wal/colors-wal-dmenu.h"
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=14"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/*
+static const char *colors[SchemeLast][2] = {
+	//     fg         bg       
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeHp] = { "#bbbbbb", "#333333" }
+};
+*/
+
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 5;
+static unsigned int lineheight      = 0;
+static unsigned int columns    = 5;
+
+static int dmx = 0; /* put dmenu at this x offset */
+static int dmy = 40; /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
+static unsigned int dmw = 2550; /* make dmenu this wide */
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
